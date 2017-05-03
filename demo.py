cde2393a64c5d342745fd434c5da04f99f5a685b
Derek Headley <champstank@Dereks-MacBook-Pro.local>
2017-05-03 04:10:30 -0600

    Adding overall status messaging into demo

--- demo.py
+++ demo.py
@@ -1,12 +1,21 @@
 from dereksdocker import dereksdocker
 import glob
-
-for file in glob.glob('*'):
-	print file
+import time
+import numpy as np
 
 filenames = glob.glob('examples/*')
 
-print filenames 
-
+success=[]
+start_time = time.time()
 for filename in filenames:
-	dereksdocker(filename=filename).run()
+  status = dereksdocker(filename=filename).run()
+  success.append(status)
+success = np.array(success)  # needed for counts later
+end_time = time.time()
+delta_time = end_time - start_time  # how long to run, seconds
+
+print(" <---EXAMPLES FINISHED in "+str(delta_time)+"s--->")
+good_files = np.sum(success==True)
+total_files = len(filenames)
+
+print(" <---"+str(good_files)+"/"+str(total_files)+" completed successfully!")
